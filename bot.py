55b66ac809a3b57a7ed7cc32fa7b35f55d567af2
Sree Hari <98310399+Srehrix@users.noreply.github.com>
2022-11-25 16:31:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,14 +11,13 @@ from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, PORT
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 from aiohttp import web
 from plugins import web_server
 
-PORT = "8080"
 
 class Bot(Client):
 
